b25a5b6ddb23bc1913532d61b9da2e99dec0ae9c
luis-camus <163400348+luis-camus@users.noreply.github.com>
2024-04-11 14:19:07 +0800

    working on feature with ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter/counter.component';
 import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
 import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter/state/counter.reducer';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,7 @@ import { CounterButtonsComponent } from './counter/counter-buttons/counter-butto
     CounterOutputComponent,
     CounterButtonsComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({ counter: counterReducer })],
   providers: [],
   bootstrap: [AppComponent],
 })
